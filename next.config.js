8a48ba7ac4bbec9a55880d9629c947780737c8c9
Vincent Llanto <72075830+centavosx@users.noreply.github.com>
2022-12-05 23:46:56 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  assetPrefix:
-    process.env.NODE_ENV === 'development'
-      ? undefined
-      : 'https://manila-feline.netlify.app',
+  assetPrefix: 'https://manila-feline.netlify.app',
   reactStrictMode: true,
   swcMinify: true,
   serverRuntimeConfig: {
